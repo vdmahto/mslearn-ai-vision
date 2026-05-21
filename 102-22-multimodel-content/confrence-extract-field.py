b0fd5cbc07db6ca997fa3c0e4d414f0be2d100e0
unknown <vdmahto@hotmail.com>
2026-05-20 22:17:17 -0700

    conf-extract-field

---
+++ 102-22-multimodel-content/confrence-extract-field.py
@@ -0,0 +1,229 @@
+import json
+import logging
+import sys
+import time
+from collections.abc import Callable
+from pathlib import Path
+from typing import Any, cast
+from dataclasses import dataclass
+
+import requests
+
+
+def main():
+    settings = Settings(
+        endpoint="https://ai-hub61993221413002386233.cognitiveservices.azure.com/",
+        api_version="2025-05-01-preview",
+        # Either subscription_key or aad_token must be provided. Subscription Key is more prioritized.
+        subscription_key="AZURE_CONTENT_UNDERSTANDING_SUBSCRIPTION_KEY",
+        aad_token="AZURE_CONTENT_UNDERSTANDING_AAD_TOKEN",
+        # Insert the analyzer name.
+        analyzer_id="conference-call-analyzer",
+        # Insert the supported file types of the analyzer.
+        file_location="https://raw.githubusercontent.com/Azure/azure-sdk-for-python/main/sdk/formrecognizer/azure-ai-formrecognizer/tests/sample_forms/receipt/contoso-receipt.png",
+    )
+    client = AzureContentUnderstandingClient(
+        settings.endpoint,
+        settings.api_version,
+        subscription_key=settings.subscription_key,
+        token_provider=settings.token_provider,
+    )
+    response = client.begin_analyze(settings.analyzer_id, settings.file_location)
+    result = client.poll_result(
+        response,
+        timeout_seconds=60 * 60,
+        polling_interval_seconds=1,
+    )
+    json.dump(result, sys.stdout, indent=2)
+
+
+@dataclass(frozen=True, kw_only=True)
+class Settings:
+    endpoint: str
+    api_version: str
+    subscription_key: str | None = None
+    aad_token: str | None = None
+    analyzer_id: str
+    file_location: str
+
+    def __post_init__(self):
+        key_not_provided = (
+            not self.subscription_key
+            or self.subscription_key == "AZURE_CONTENT_UNDERSTANDING_SUBSCRIPTION_KEY"
+        )
+        token_not_provided = (
+            not self.aad_token
+            or self.aad_token == "AZURE_CONTENT_UNDERSTANDING_AAD_TOKEN"
+        )
+        if key_not_provided and token_not_provided:
+            raise ValueError(
+                "Either 'subscription_key' or 'aad_token' must be provided"
+            )
+
+    @property
+    def token_provider(self) -> Callable[[], str] | None:
+        aad_token = self.aad_token
+        if aad_token is None:
+            return None
+
+        return lambda: aad_token
+
+
+class AzureContentUnderstandingClient:
+    def __init__(
+        self,
+        endpoint: str,
+        api_version: str,
+        subscription_key: str | None = None,
+        token_provider: Callable[[], str] | None = None,
+        x_ms_useragent: str = "cu-sample-code",
+    ) -> None:
+        if not subscription_key and token_provider is None:
+            raise ValueError(
+                "Either subscription key or token provider must be provided"
+            )
+        if not api_version:
+            raise ValueError("API version must be provided")
+        if not endpoint:
+            raise ValueError("Endpoint must be provided")
+
+        self._endpoint: str = endpoint.rstrip("/")
+        self._api_version: str = api_version
+        self._logger: logging.Logger = logging.getLogger(__name__)
+        self._logger.setLevel(logging.INFO)
+        self._headers: dict[str, str] = self._get_headers(
+            subscription_key, token_provider and token_provider(), x_ms_useragent
+        )
+
+    def begin_analyze(self, analyzer_id: str, file_location: str):
+        """
+        Begins the analysis of a file or URL using the specified analyzer.
+
+        Args:
+            analyzer_id (str): The ID of the analyzer to use.
+            file_location (str): The path to the file or the URL to analyze.
+
+        Returns:
+            Response: The response from the analysis request.
+
+        Raises:
+            ValueError: If the file location is not a valid path or URL.
+            HTTPError: If the HTTP request returned an unsuccessful status code.
+        """
+        if Path(file_location).exists():
+            with open(file_location, "rb") as file:
+                data = file.read()
+            headers = {"Content-Type": "application/octet-stream"}
+        elif "https://" in file_location or "http://" in file_location:
+            data = {"url": file_location}
+            headers = {"Content-Type": "application/json"}
+        else:
+            raise ValueError("File location must be a valid path or URL.")
+
+        headers.update(self._headers)
+        if isinstance(data, dict):
+            response = requests.post(
+                url=self._get_analyze_url(
+                    self._endpoint, self._api_version, analyzer_id
+                ),
+                headers=headers,
+                json=data,
+            )
+        else:
+            response = requests.post(
+                url=self._get_analyze_url(
+                    self._endpoint, self._api_version, analyzer_id
+                ),
+                headers=headers,
+                data=data,
+            )
+
+        response.raise_for_status()
+        self._logger.info(
+            f"Analyzing file {file_location} with analyzer: {analyzer_id}"
+        )
+        return response
+
+    def poll_result(
+        self,
+        response: requests.Response,
+        timeout_seconds: int = 120,
+        polling_interval_seconds: int = 2,
+    ) -> dict[str, Any]:  # pyright: ignore[reportExplicitAny]
+        """
+        Polls the result of an asynchronous operation until it completes or times out.
+
+        Args:
+            response (Response): The initial response object containing the operation location.
+            timeout_seconds (int, optional): The maximum number of seconds to wait for the operation to complete. Defaults to 120.
+            polling_interval_seconds (int, optional): The number of seconds to wait between polling attempts. Defaults to 2.
+
+        Raises:
+            ValueError: If the operation location is not found in the response headers.
+            TimeoutError: If the operation does not complete within the specified timeout.
+            RuntimeError: If the operation fails.
+
+        Returns:
+            dict: The JSON response of the completed operation if it succeeds.
+        """
+        operation_location = response.headers.get("operation-location", "")
+        if not operation_location:
+            raise ValueError("Operation location not found in response headers.")
+
+        headers = {"Content-Type": "application/json"}
+        headers.update(self._headers)
+
+        start_time = time.time()
+        while True:
+            elapsed_time = time.time() - start_time
+            self._logger.info(
+                "Waiting for service response", extra={"elapsed": elapsed_time}
+            )
+            if elapsed_time > timeout_seconds:
+                raise TimeoutError(
+                    f"Operation timed out after {timeout_seconds:.2f} seconds."
+                )
+
+            response = requests.get(operation_location, headers=self._headers)
+            response.raise_for_status()
+            result = cast(dict[str, str], response.json())
+            status = result.get("status", "").lower()
+            if status == "succeeded":
+                self._logger.info(
+                    f"Request result is ready after {elapsed_time:.2f} seconds."
+                )
+                return response.json()  # pyright: ignore[reportAny]
+            elif status == "failed":
+                self._logger.error(f"Request failed. Reason: {response.json()}")
+                raise RuntimeError("Request failed.")
+            else:
+                self._logger.info(
+                    f"Request {operation_location.split('/')[-1].split('?')[0]} in progress ..."
+                )
+            time.sleep(polling_interval_seconds)
+
+    def _get_analyze_url(self, endpoint: str, api_version: str, analyzer_id: str):
+        return f"{endpoint}/contentunderstanding/analyzers/{analyzer_id}:analyze?api-version={api_version}&stringEncoding=utf16"
+
+    def _get_headers(
+        self, subscription_key: str | None, api_token: str | None, x_ms_useragent: str
+    ) -> dict[str, str]:
+        """Returns the headers for the HTTP requests.
+        Args:
+            subscription_key (str): The subscription key for the service.
+            api_token (str): The API token for the service.
+            enable_face_identification (bool): A flag to enable face identification.
+        Returns:
+            dict: A dictionary containing the headers for the HTTP requests.
+        """
+        headers = (
+            {"Ocp-Apim-Subscription-Key": subscription_key}
+            if subscription_key
+            else {"Authorization": f"Bearer {api_token}"}
+        )
+        headers["x-ms-useragent"] = x_ms_useragent
+        return headers
+
+
+if __name__ == "__main__":
+    main()
